06749f1c5486d97d6f9613fd5d4e8c6174efbae7
Naufal Milzam <82256293+CrustFast@users.noreply.github.com>
2025-11-06 20:24:04 +0700

    add hero component & pengaduan component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { ReactLenis } from 'lenis/react';
 import "./globals.css";
 
 const geistSans = Geist({
@@ -25,9 +26,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-white min-h-screen`}
       >
-        {children}
+        <ReactLenis root options={{ duration: 1.2, smoothWheel: true }}>
+          {children}
+        </ReactLenis>
       </body>
     </html>
   );
