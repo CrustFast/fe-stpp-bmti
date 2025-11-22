07154af015f4cc393cc976e060d08d2d34ce6446
Naufal Milzam <82256293+CrustFast@users.noreply.github.com>
2025-11-22 12:04:20 +0700

    add cors and connected to backend API

---
+++ lib/api.ts
@@ -0,0 +1,7 @@
+// lib/api.ts
+export const API_URL = process.env.NODE_ENV === "production"
+  ? "https://fe-stpp-bmti.vercel.app"
+  : "http://localhost:8080";
+
+export const PUBLIC_API_BASE =
+  process.env.NEXT_PUBLIC_API_BASE || "http://localhost:8080";
